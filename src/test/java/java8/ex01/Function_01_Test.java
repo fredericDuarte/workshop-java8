1bbe9654fd29acc0165c1f4427cb7db45dc445fc
Fred <geforst@gmail.com>
2019-02-13 23:31:32 +0100

    03 completed

--- src/test/java/java8/ex01/Function_01_Test.java
+++ src/test/java/java8/ex01/Function_01_Test.java
@@ -9,92 +9,99 @@ import java.util.function.Function;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-
 /**
  * Exercice 01 - java.util.function.Function
  */
 public class Function_01_Test {
 
-    /******** PART 1 - Integer -> Person *******/
+	/******** PART 1 - Integer -> Person *******/
 
-    // tag::intToPerson[]
-    // TODO Compléter la définition de cette fonction
-    // TODO Cette fonction permet de transformer un entier en objet Person
-    // TODO le prenom sera de la forme "first_<ENTIER>"
-    // TODO le nom sera de la forme "last_<ENTIER>"
-    // TODO l'age sera de la forme "<ENTIER>"
-    // TODO le mot de passe sera de la forme "pass_<ENTIER>"
-    private Function<Integer, Person> intToPerson = null;
-    // end::intToPerson[]
+	// tag::intToPerson[]
+	// TODO Compléter la définition de cette fonction
+	// TODO Cette fonction permet de transformer un entier en objet Person
+	// TODO le prenom sera de la forme "first_<ENTIER>"
+	// TODO le nom sera de la forme "last_<ENTIER>"
+	// TODO l'age sera de la forme "<ENTIER>"
+	// TODO le mot de passe sera de la forme "pass_<ENTIER>"
 
-    @Test
-    public void test_intToPerson() throws Exception {
+	private Function<Integer, Person> intToPerson = (a) -> new Person("first_" + a, "last_" + a, a, "pass_" + a);
+	// end::intToPerson[]
 
-        // TODO invoquer la fonction intToPerson avec en paramètre l'entier 10.
-        Person result = null;
+	@Test
+	public void test_intToPerson() throws Exception {
 
-        assertThat(result, hasProperty("firstname", is("first_10")));
-        assertThat(result, hasProperty("lastname", is("last_10")));
-        assertThat(result, hasProperty("age", is(10)));
-        assertThat(result, hasProperty("password", is("pass_10")));
-    }
+		// TODO invoquer la fonction intToPerson avec en paramètre l'entier 10.
 
-    /******** PART 2 - Person -> Account *******/
+		Person result = intToPerson.apply(10);
 
-    // tag::personToAccount[]
-    // TODO Compléter la définition de cette fonction
-    // TODO la propriété owner est valorisé avec la personne en paramètre
-    // TODO la propriété balance est valorisé à 1000
-    private Function<Person, Account> personToAccount = null;
-    // end::personToAccount[]
+		assertThat(result, hasProperty("firstname", is("first_10")));
+		assertThat(result, hasProperty("lastname", is("last_10")));
+		assertThat(result, hasProperty("age", is(10)));
+		assertThat(result, hasProperty("password", is("pass_10")));
+	}
 
-    @Test
-    public void test_personToAccount() throws Exception {
+	/******** PART 2 - Person -> Account *******/
 
-        Person person = new Person("Jules", "France", 10, "pass");
+	// tag::personToAccount[]
+	// TODO Compléter la définition de cette fonction
+	// TODO la propriété owner est valorisé avec la personne en paramètre
+	// TODO la propriété balance est valorisé à 1000
+	private Function<Person, Account> personToAccount = p -> {
+		Account acc = new Account();
+		acc.setBalance(1000);
+		acc.setOwner(p);
+		return acc;
+	};
+	// end::personToAccount[]
 
-        // TODO invoquer la fonction personToAccount
-        Account result = null;
+	@Test
+	public void test_personToAccount() throws Exception {
 
-        assertThat(result, hasProperty("owner", is(person)));
-        assertThat(result, hasProperty("balance", is(1000)));
-    }
+		Person person = new Person("Jules", "France", 10, "pass");
 
+		// TODO invoquer la fonction personToAccount
+		Account result = personToAccount.apply(person);
 
-    /******** PART 3 - Integer -> Account avec compose *******/
+		assertThat(result, hasProperty("owner", is(person)));
+		assertThat(result, hasProperty("balance", is(1000)));
+	}
 
-    // tag::intToAccountWithCompose[]
-    // TODO Compléter la définition de cette fonction
-    // TODO Utiliser la méthode compose pour réutiliser les fonctions intToPerson et personToAccount
-    private Function<Integer, Account> intToAccountWithCompose = null;
-    // end::intToAccountWithCompose[]
+	/******** PART 3 - Integer -> Account avec compose *******/
 
+	// tag::intToAccountWithCompose[]
+	// TODO Compléter la définition de cette fonction
+	// TODO Utiliser la méthode compose pour réutiliser les fonctions intToPerson et
+	// personToAccount
+	//private Function<Integer, Account> intToAccountWithCompose = intToPerson.compose(personToAccount);
+	private Function<Integer, Account> intToAccountWithCompose = intToPerson.andThen(personToAccount);
+	// end::intToAccountWithCompose[]
 
-    @Test
-    public void test_intToAccount_with_Compose() throws Exception {
+	@Test
+	public void test_intToAccount_with_Compose() throws Exception {
 
-        // TODO invoquer la fonction intToAccountWithCompose avec l'entier 10
-        Account result = null;
+		// TODO invoquer la fonction intToAccountWithCompose avec l'entier 10
+		Account result = intToAccountWithCompose.apply(10);
 
-        assertThat(result.getOwner(), hasProperty("firstname", is("first_10")));
-        assertThat(result, hasProperty("balance", is(1000)));
-    }
+		assertThat(result.getOwner(), hasProperty("firstname", is("first_10")));
+		assertThat(result, hasProperty("balance", is(1000)));
+	}
 
-    /******** PART 4 - Integer -> Account avec andThen *******/
+	/******** PART 4 - Integer -> Account avec andThen *******/
 
-    // tag::intToAccountWithAndThen[]
-    // TODO Compléter la définition de cette fonction
-    // TODO Utiliser la méthode andThen pour réutiliser les fonctions intToPerson et personToAccount
-    private Function<Integer, Account> intToAccountWithAndThen = null;
-    // end::intToAccountWithAndThen[]
+	// tag::intToAccountWithAndThen[]
+	// TODO Compléter la définition de cette fonction
+	// TODO Utiliser la méthode andThen pour réutiliser les fonctions intToPerson et
+	// personToAccount
+	private Function<Integer, Account> intToAccountWithAndThen = intToPerson.andThen(personToAccount);
+	// end::intToAccountWithAndThen[]
 
-    @Test
-    public void test_intToAccount_with_AndThen() throws Exception {
+	@Test
+	public void test_intToAccount_with_AndThen() throws Exception {
 
-        // TODO invoquer la fonction intToAccountWithAndThen avec l'entier 11
-        Account result = null;
+		// TODO invoquer la fonction intToAccountWithAndThen avec l'entier 11
+		Account result = intToAccountWithAndThen.apply(11);
 
-        assertThat(result.getOwner(), hasProperty("firstname", is("first_11")));
-        assertThat(result, hasProperty("balance", is(1000)));
-    }
+		assertThat(result.getOwner(), hasProperty("firstname", is("first_11")));
+		assertThat(result, hasProperty("balance", is(1000)));
+	}
 }
--- src/test/java/java8/ex07/Function_07_Test.java
+++ src/test/java/java8/ex07/Function_07_Test.java
@@ -12,37 +12,34 @@ import static org.junit.Assert.*;
  */
 public class Function_07_Test {
 
-    // tag::format[]
-    // TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>"
-    // TODO ex. "(10+11)=21", "(5-2)=3"
-    String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
-        // TODO
-        return null;
-    }
-    // end::format[]
+	// tag::format[]
+	// TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la
+	// forme "(<nb1><symbol><nb2>)=<resultat>"
+	// TODO ex. "(10+11)=21", "(5-2)=3"
+	String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
+		return "(" + nb1 + symbol + nb2 + ")=" + (operator.applyAsInt(nb1, nb2));
+	}
+	// end::format[]
 
-    // TODO définir sum pour que le test test_format_sum() soit passant
-    IntBinaryOperator sum = null;
+	// TODO définir sum pour que le test test_format_sum() soit passant
+	IntBinaryOperator sum = (nb1, nb2) -> nb1 + nb2;
 
-    @Test
-    public void test_format_sum() throws Exception {
+	@Test
+	public void test_format_sum() throws Exception {
 
-        String result = format(12, 13, "+", sum);
+		String result = format(12, 13, "+", sum);
 
-        assertThat(result, is("(12+13)=25"));
-    }
+		assertThat(result, is("(12+13)=25"));
+	}
 
-    // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+	// TODO définir substract afin que le test test_format_subtract() soit passant
+	IntBinaryOperator substract = (nb1, nb2) -> nb1 - nb2;
 
-    @Test
-    public void test_format_subtract() throws Exception {
+	@Test
+	public void test_format_subtract() throws Exception {
 
-        String result = format(2, 3, "-", substract);
+		String result = format(2, 3, "-", substract);
 
-        assertThat(result, is("(2-3)=-1"));
-    }
+		assertThat(result, is("(2-3)=-1"));
+	}
 }
-
-
-
